d676c2f91005e5b2094f3936a7cb02fd01bc2bbe
Saiyadul77 <saiyadul77@gmail.com>
2022-05-18 17:52:23 +0600

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const app = express();
@@ -20,27 +20,27 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const userCollection = client.db("warehouse").collection("items");
-        
+        const serviceCollection = client.db("warehouse").collection("items");
 
-        app.get('/user', async(req,res)=>{
-            const query={};
-            const cursor= userCollection.find(query);
-            const users= await cursor.toArray();
-            res.send(users);
+// multiple data get
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
         })
-
+// single data get
         app.get('/service/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const service = await serviceCollection.findOne(query);
-      res.send(service);
-    })
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
         // POST USER
-        app.post('/user', async(req,res)=>{
-            const newUser= req.body;
-            console.log('adding new user',newUser);
-            const result= await userCollection.insertOne(newUser);
+        app.post('/user', async (req, res) => {
+            const newUser = req.body;
+            console.log('adding new user', newUser);
+            const result = await serviceCollection.insertOne(newUser);
             res.send(result)
         })
 
