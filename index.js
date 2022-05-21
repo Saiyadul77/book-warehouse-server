9e42f41e0a8b7ba95e508e30865b46dc2a21d092
Saiyadul77 <saiyadul77@gmail.com>
2022-05-21 15:56:46 +0600

    updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -22,26 +23,53 @@ async function run() {
         await client.connect();
         const serviceCollection = client.db("warehouse").collection("items");
 
-// multiple data get
+        //AUTH
+
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+        // multiple data get
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
-// single data get
+        // single data get
         app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
         })
+
         // POST USER
-        app.post('/user', async (req, res) => {
+        app.post('/service', async (req, res) => {
             const newUser = req.body;
             console.log('adding new user', newUser);
             const result = await serviceCollection.insertOne(newUser);
             res.send(result)
+        })
+
+        // Delete 
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // Data decrease and update
+
+        app.put('/service/:id', (req, res) => {
+            const id = req.params.id;
+            const result = serviceCollection.findOneAndUpdate(id);
+            res.send(result);
         })
 
     }
