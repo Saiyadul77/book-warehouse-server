26a004720aa3fd568e2da31f60d12570a9c669ac
Saiyadul77 <saiyadul77@gmail.com>
2022-06-05 01:57:37 +0600

    changed repository

--- index.js
+++ index.js
@@ -12,6 +12,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: 'unauthorized access' });
+    }
+  
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      console.log('decoded', decoded);
+      req.decoded = decoded;
+      next();
+    })
+    // console.log('inside verifier', authHeader);
+    
+  }
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e04nu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -22,6 +40,7 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("warehouse").collection("items");
+        const userCollection = client.db("warehouse").collection("users");
 
         //AUTH
 
@@ -41,7 +60,7 @@ async function run() {
             res.send(services);
         })
         // single data get
-        app.get('/service/:id', async (req, res) => {
+        app.get('/service/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
